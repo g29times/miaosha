ea6eade4cd8f795d897bb2314c88511dd9b69139
Tony Lee <g29tny@foxmail.com>
2018-06-27 22:27:41 +0800

    秒杀失败跳转处理

--- workspace/miaosha_3/src/main/java/com/imooc/miaosha/controller/MiaoshaController.java
+++ workspace/miaosha_3/src/main/java/com/imooc/miaosha/controller/MiaoshaController.java
@@ -1,11 +1,5 @@
 package com.imooc.miaosha.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Controller;
-import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
-
 import com.imooc.miaosha.domain.MiaoshaOrder;
 import com.imooc.miaosha.domain.MiaoshaUser;
 import com.imooc.miaosha.domain.OrderInfo;
@@ -16,50 +10,69 @@ import com.imooc.miaosha.service.MiaoshaService;
 import com.imooc.miaosha.service.MiaoshaUserService;
 import com.imooc.miaosha.service.OrderService;
 import com.imooc.miaosha.vo.GoodsVo;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
 
 @Controller
 @RequestMapping("/miaosha")
 public class MiaoshaController {
 
-	@Autowired
-	MiaoshaUserService userService;
-	
-	@Autowired
-	RedisService redisService;
-	
-	@Autowired
-	GoodsService goodsService;
-	
-	@Autowired
-	OrderService orderService;
-	
-	@Autowired
-	MiaoshaService miaoshaService;
-	
+    @Autowired
+    MiaoshaUserService userService;
+
+    @Autowired
+    RedisService redisService;
+
+    @Autowired
+    GoodsService goodsService;
+
+    @Autowired
+    OrderService orderService;
+
+    @Autowired
+    MiaoshaService miaoshaService;
+
     @RequestMapping("/do_miaosha")
-    public String list(Model model,MiaoshaUser user,
-    		@RequestParam("goodsId")long goodsId) {
-    	model.addAttribute("user", user);
-    	if(user == null) {
-    		return "login";
-    	}
-    	//判断库存
-    	GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
-    	int stock = goods.getStockCount();
-    	if(stock <= 0) {
-    		model.addAttribute("errmsg", CodeMsg.MIAO_SHA_OVER.getMsg());
-    		return "miaosha_fail";
-    	}
-    	//判断是否已经秒杀到了
-    	MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
-    	if(order != null) {
-    		model.addAttribute("errmsg", CodeMsg.REPEATE_MIAOSHA.getMsg());
-    		return "miaosha_fail";
-    	}
-    	//减库存 下订单 写入秒杀订单
-    	OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
-    	model.addAttribute("orderInfo", orderInfo);
-    	model.addAttribute("goods", goods);
+    public String list(Model model, MiaoshaUser user,
+                       @RequestParam("goodsId") long goodsId) {
+        model.addAttribute("user", user);
+        if (user == null) {
+            return "login";
+        }
+        //判断库存
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(goodsId);
+        int stock = goods.getStockCount();
+        if (stock <= 0) {
+            model.addAttribute("errmsg", CodeMsg.MIAO_SHA_OVER.getMsg());
+            return "miaosha_fail";
+        }
+        //判断是否已经秒杀到了
+        MiaoshaOrder order = orderService.getMiaoshaOrderByUserIdGoodsId(user.getId(), goodsId);
+        if (order != null) {
+            model.addAttribute("errmsg", CodeMsg.REPEATE_MIAOSHA.getMsg());
+            model.addAttribute("miaoshaOrderId", order.getOrderId());
+            model.addAttribute("miaoshaGoodsId", goodsId);
+            return "miaosha_fail";
+        }
+        //减库存 下订单 写入秒杀订单
+        OrderInfo orderInfo = miaoshaService.miaosha(user, goods);
+        model.addAttribute("orderInfo", orderInfo);
+        model.addAttribute("goods", goods);
+        return "order_detail";
+    }
+
+    @RequestMapping("/to_order")
+    public String backToOrder(Model model,
+                              @RequestParam("orderId") String orderId,
+                              @RequestParam("goodsId") String goodsId) {
+        OrderInfo orderInfo = orderService.getOrderById(Long.valueOf(orderId));
+        GoodsVo goods = goodsService.getGoodsVoByGoodsId(Long.valueOf(goodsId));
+        model.addAttribute("orderInfo", orderInfo);
+        model.addAttribute("goods", goods);
         return "order_detail";
     }
 }
